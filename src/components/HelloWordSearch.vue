8a0d6365db3df4e69dc196b5fd8c850f317f523f
rachmathidayat.dev <rachmathidayat.dev@gmail.com>
2018-08-21 14:59:14 +0700

    update

--- src/components/HelloWordSearch.vue
+++ src/components/HelloWordSearch.vue
@@ -1,6 +1,6 @@
 <template id="HelloWordSearch">
   <ons-page id="tabbar-page">
-    <ons-toolbar style="background: rgb(255, 0, 0) !important">
+    <ons-toolbar style="background: rgb(237, 24, 73) !important">
       <div class="left">
         <router-link to="/">
         <ons-toolbar-button style="color: white !important">
@@ -9,38 +9,41 @@
         </router-link>
       </div>
       <div class="center" style="color: white !important">Home</div>
-      <div class="right">
-        <ons-toolbar-button style="color: white !important">
-          <ons-icon icon="ion-navicon, material:md-menu"></ons-icon>
-        </ons-toolbar-button>
-      </div>
     </ons-toolbar>
 
     <ons-card @click="detail">
-      <div class="title">{{ nameCountry }}</div>
-      <div class="content">
-        <img v-bind:src="flag" style="height: 50px; width: 50px">
-      </div>
-      <div class="content">subregion: {{ subregionCountry }}</div>
-      <div class="content">region: {{ regionCountry }}</div>
-      <div class="content">timezone: {{ timezoneCountry }}</div>
-      <div class="content">timezone: {{ currencyCountry }}</div>
-      <div class="content">timezone: {{ languagesCountry }}</div>
+        <div class="title" v-if="nameCountry !== ''" style="display: flex">
+            <img v-bind:src="flag" style="height: 50px; width: 50px"> 
+            <span style="margin-top: 15px; margin-left: 15px; font-weight: bold">  {{ nameCountry }}</span>
+        </div>
     </ons-card>
 
-    <ons-modal direction="up">
+    <ons-dialog direction="up">
         <ons-card @click="hideDetail">
-            <div class="content" style="font-size: 28px">{{ nameCountry }}</div>
-            <div class="content">
+            <div class="content" v-if="nameCountry !== ''" style="font-size: 28px">{{ nameCountry }}</div>
+            <div class="content" v-else></div>
+            <div class="content" v-if="flag !== ''">
                 <img v-bind:src="flag" style="height: 50px; width: 50px">
             </div>
-            <div class="content">subregion: {{ subregionCountry }}</div>
-            <div class="content">region: {{ regionCountry }}</div>
-            <div class="content">timezone: {{ timezoneCountry }}</div>
-            <div class="content">timezone: {{ currencyCountry }}</div>
-            <div class="content">timezone: {{ languagesCountry }}</div>
+            <div class="content" v-else></div>
+            <div class="content" v-if="subregionCountry !== ''">subregion: {{ subregionCountry }}</div>
+            <div class="content" v-else></div>
+            <div class="content" v-if="regionCountry !== ''">region: {{ regionCountry }}</div>
+            <div class="content" v-else></div>
+            <div class="content" v-if="timezoneCountry !== ''">timezone: {{ timezoneCountry }}</div>
+            <div class="content" v-else></div>
+            <div class="content" v-if="currencyCountry !== ''">currency: {{ currencyCountry }}</div>
+            <div class="content" v-else></div>
+            <div class="content" v-if="languagesCountry !== ''">language: {{ languagesCountry }}</div>
+            <div class="content" v-else></div>
         </ons-card>
-    </ons-modal>
+    </ons-dialog>
+
+    <ons-dialog direction="up" id="errormodal" @click="hideDetailError">
+        <div style="display: flex">
+            <span><div class="content" style="margin-left: 80px; margin-top: 35px; font-weight: bold; color: rgb(237, 24, 73);">Maaf, data tidak ditemukan</div></span>
+        </div>
+    </ons-dialog>        
 
   </ons-page>
 </template>
@@ -81,18 +84,33 @@ export default {
                 this.currencyCountry = this.dataCountry[0].currencies[0].name
                 this.languagesCountry = this.dataCountry[0].languages[0].name
                 this.flag = this.dataCountry[0].flag
+            }else{
+                
             }
+        })
+        .catch((err) => {
+            var modal = document.getElementById('errormodal');
+            modal.show();
         })
       },
 
       detail: function() {
-          var modal = document.querySelector('ons-modal');
-          modal.show();
+          var modal = document.querySelector('ons-dialog');
+          if(this.nameCountry !== ''){
+              modal.show();
+          }
       },
 
       hideDetail: function() {
-          var modal = document.querySelector('ons-modal');
+          var modal = document.querySelector('ons-dialog');
           modal.hide();
+      },
+
+      hideDetailError: function() {
+        //   var modal = document.getElementById('errormodal');
+        //   modal.hide();
+        //   return false
+        this.$router.push({ name: 'HelloWorld' })
       }
   }
 }  
@@ -100,5 +118,8 @@ export default {
 <style>
     .toolbar--material toolbar{
         background: red !important
+    }
+    .dialog-container.dialog-container--material{
+        width: 350px !important;
     }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,7 @@
 <template id="HelloWorld">
   <ons-page id="tabbar-page">
-    <ons-toolbar style="background: rgb(255, 0, 0) !important">
+    <ons-toolbar style="background: rgb(237, 24, 73) !important">
       <div class="center" style="color: white !important">Home</div>
-      <div class="right">
-        <ons-toolbar-button style="color: white !important">
-          <ons-icon icon="ion-navicon, material:md-menu"></ons-icon>
-        </ons-toolbar-button>
-      </div>
     </ons-toolbar>
 
     <ons-card style="height: 570px !important">
@@ -20,12 +15,10 @@
 
           <ons-list-item class="input-items">
             <label class="center">
-              <v-ons-search-input id="name-input" float maxlength="20" placeholder="Search data" v-model="searchParam"></v-ons-search-input>
+              <v-ons-search-input id="name-input" float maxlength="20" placeholder="Search data" v-model="searchParam" v-on:keyup.enter="onEnter"></v-ons-search-input>
             </label>
             <div class="right">
-              <ons-button class="button" v-on:click="searchDatanya" style="background: rgb(255, 0, 0) !important">
-                <ons-icon icon="md-search" class="ons-icon zmdi zmdi-search"></ons-icon>
-              </ons-button>
+                <ons-icon icon="md-search" v-on:click="searchDatanya" class="ons-icon zmdi zmdi-search" style="color: black; font-size: 36px;"></ons-icon>
             </div>
           </ons-list-item>
 
@@ -51,6 +44,10 @@ export default {
 
   methods: {
     searchDatanya: function() {
+      let parameter = this.searchParam
+      this.$router.push({ name: 'HelloWordSearch', params: {parameter: parameter} })
+    },
+    onEnter: function() {
       let parameter = this.searchParam
       this.$router.push({ name: 'HelloWordSearch', params: {parameter: parameter} })
     }
